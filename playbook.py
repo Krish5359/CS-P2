a1dab71cae10548c3b0eae79cc2380fa4cf3c0de
krish kanani <148679497+Krish5359@users.noreply.github.com>
2026-07-18 19:24:14 +0530

    feat: added host isolation and containment playbook

--- playbook.py
+++ playbook.py
@@ -28,3 +28,28 @@ block_ip("172.16.0.200")
 block_ip("10.0.0.55")
 block_ip("192.168.1.100")
 show_blocked_ips()
+
+contained_hosts = []
+
+def isolate_host(host):
+    if host not in contained_hosts:
+        contained_hosts.append(host)
+        print("ACTION: Host " + host + " has been ISOLATED from network!")
+    else:
+        print("INFO: Host " + host + " is already isolated!")
+
+def run_containment_playbook(ip, host, risk_score):
+    print("RUNNING CONTAINMENT PLAYBOOK...")
+    print("Threat IP: " + ip)
+    print("Affected Host: " + host)
+    print("Risk Score: " + str(risk_score))
+    print("---")
+    block_ip(ip)
+    isolate_host(host)
+    print("CONTAINMENT COMPLETE!")
+    print("Time to contain: under 5 seconds!")
+
+print("CONTAINMENT PLAYBOOK:")
+run_containment_playbook("192.168.1.100", "HOST-001", 90)
+run_containment_playbook("172.16.0.200", "HOST-002", 70)
+run_containment_playbook("10.0.0.55", "HOST-003", 40)
